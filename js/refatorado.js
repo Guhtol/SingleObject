ddcb1c0b32d02e8e4580c2564b4f194d1230707d
Guhtol <programador.bueno@gmail.com>
2017-01-05 20:27:58 -0200

    refatorado para ecma06

--- js/refatorado.js
+++ js/refatorado.js
@@ -1,42 +1,17 @@
 (function (root) {
 
-    root.sgObject = {
-        create: create,
-        setAtt: setAttributes
+    const createFrag = () => document.createDocumentFragment()
+
+    const makeReduce = fn => param => array => array.reduce(fn, param)
+
+    const getNameProperty = obj => Object.keys(obj)
+
+    const createAttribute = (acc, item) => {
+        acc.element.setAttribute(item, acc.obj[item])
+        return acc
     }
 
-    function createFrag() {
-        return document.createDocumentFragment()
-    }
-
-    function create(tagName) {
-        return function (obj) {
-            var element = document.createElement(tagName)
-
-            if (element === null) {
-                throw new Error('tag name not valid')
-            }
-
-            var reduceEl = reduce(configElement)
-            var createEl = reduceEl({ element: element, obj: obj })
-
-            return createEl(getNamesProperty(obj)).element
-        }
-    }
-    function setAttributes(element) {
-        return function (obj) {
-            var reduceEl = reduce(createAttribute)
-            var createEl = reduceEl({ element: element, obj: obj })
-
-            return createEl(getNamesProperty(obj)).element
-        }
-    }
-
-    function getNamesProperty(obj) {
-        return Object.keys(obj)
-    }
-
-    function configElement(acc, item) {
+    const configElement = (acc, item) => {
         switch (item) {
             case "class":
                 acc.element['className'] = acc.obj[item]
@@ -48,17 +23,29 @@
         }
     }
 
-    function createAttribute(acc, item) {
-        acc.element.setAttribute(item, acc.obj[item])
-        return acc
+    const makeElement = tagName => obj => {
+        const element = document.createElement(tagName)
+
+        if (element === null)
+            throw new Error('tag name not valid')
+
+        const mkReduce = makeReduce(configElement)
+        const createEl = mkReduce({ element, obj })
+
+        return createEl(getNameProperty(obj)).element
     }
 
-    function reduce(fn) {
-        return function (param) {
-            return function (array) {
-                return array.reduce(fn, param)
-            }
-        }
+    const makeSetAttribute = element => obj => {
+        const mkReduce = makeReduce(createAttribute)
+        const createEl = mkReduce({ element, obj })
+
+        return createEl(getNameProperty(obj)).element
     }
+
+    root.sgObject = {
+        create: makeElement,
+        setAtt: makeSetAttribute
+    }
+
 
 })(this)
